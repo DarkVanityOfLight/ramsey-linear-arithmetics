741bc68717a7b1b126a27ae763839d6650241aa4
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2026-06-26 14:37:38 +0200

    chore: Actually use the prop skeleton

--- ramsey_elimination/euf/euf_elimination.py
+++ ramsey_elimination/euf/euf_elimination.py
@@ -2,8 +2,8 @@ from typing import Tuple
 from itertools import combinations
 
 from ramsey_extensions.fnode import ExtendedFNode
-from formula_utils import _fresh_vector, collect_atoms, fresh_bool_vector
-from simplifications import make_euf_input_format
+from ramsey_elimination.formula_utils import _fresh_vector, collect_atoms, fresh_bool_vector
+from ramsey_elimination.simplifications import make_euf_input_format
 from .existential_elimination import eliminate_existential_quantifier
 FNode = ExtendedFNode
 
@@ -11,31 +11,32 @@ from pysmt.shortcuts import NotEquals, Or, And, Implies, Exists
 
 
 
-def eliminate_ramsey_euf(qFormula: ExtendedFNode) -> ExtendedFNode:
+def eliminate_ramsey_euf(qFormula: ExtendedFNode, N = 4) -> ExtendedFNode:
     assert qFormula.is_ramsey()
 
 
     eqs, _, _ = collect_atoms(qFormula)
     skeleton_vars = fresh_bool_vector("s_{}_%s", len(eqs))
 
-    skeleton = And([Implies(skeleton_vars[i], eqs[i]) for i in range(len(eqs))])
+    prop_skeleton = qFormula.arg(0).substitute({atom: skeleton_vars[i] for i, atom in enumerate(eqs)})
+    theory_con = And([Implies(skeleton_vars[i], eqs[i]) for i in range(len(eqs))])
 
     qargs: Tuple[Tuple[ExtendedFNode, ...], Tuple[ExtendedFNode, ...]] = qFormula.quantifier_vars() #type: ignore
     n = len(qargs[0])
     T = qargs[0][0].get_type()
 
-    vecs = [_fresh_vector("x_{}_%s", n, T) for _ in range(4)]
+    vecs = [_fresh_vector("x_{}_%s", n, T) for _ in range(N)]
 
     uneq = And([Or([NotEquals(x, y) for (x, y) in zip(xs, ys)]) for (xs, ys) in combinations(vecs, 2)])
 
     formals = qargs[0] + qargs[1]
 
     clique = And([
-        skeleton.substitute(dict(zip(formals, xs_p + ys_p)))
+        theory_con.substitute(dict(zip(formals, xs_p + ys_p)))
         for xs_p, ys_p in combinations(vecs, 2)
     ])
 
-    return Exists(skeleton_vars + [x for vec in vecs for x in vec], And(uneq, clique))
+    return Exists(skeleton_vars + [x for vec in vecs for x in vec], And(uneq, clique, prop_skeleton))
 
 
 def full_ramsey_elimination_euf(formula: ExtendedFNode) -> ExtendedFNode:
@@ -50,4 +51,3 @@ def full_ramsey_elimination_euf(formula: ExtendedFNode) -> ExtendedFNode:
     return eliminate_ramsey_euf(f)
 
     
-
