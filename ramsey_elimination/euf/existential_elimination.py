dd91aa6d13357512cf0fc68ef9495f2b4853fb26
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2026-06-26 14:39:15 +0200

    chore: Take in account ramsey vars when skolemizing

--- ramsey_elimination/euf/existential_elimination.py
+++ ramsey_elimination/euf/existential_elimination.py
@@ -1,7 +1,7 @@
 from typing import List, cast, Tuple
+from pysmt.shortcuts import FreshSymbol, Function, FunctionType
 from ramsey_extensions.formula import ExtendedFNode
 
-from formula_utils import skolemize
 from ramsey_extensions.shortcuts import Ramsey
 
 def eliminate_existential_quantifier(formula: ExtendedFNode) -> ExtendedFNode:
@@ -14,7 +14,11 @@ def eliminate_existential_quantifier(formula: ExtendedFNode) -> ExtendedFNode:
     ex_vars: List[ExtendedFNode] = list(formula.arg(0).quantifier_vars())
     subformula = formula.arg(0).arg(0)
 
-    sk = skolemize(subformula, set(ex_vars), ex_vars[0].get_type())
-
     x_vars, y_vars = cast(Tuple[Tuple[ExtendedFNode, ...], Tuple[ExtendedFNode, ...]], formula.quantifier_vars())
-    return Ramsey(list(x_vars), list(y_vars), sk)
+    formals = x_vars + y_vars
+    replacement = {}
+    for variable in ex_vars:
+        skolem = FreshSymbol(FunctionType(variable.get_type(), [arg.get_type() for arg in formals]))
+        replacement[variable] = Function(skolem, formals)
+
+    return Ramsey(list(x_vars), list(y_vars), subformula.substitute(replacement))
